53e8b17a17eb16f3ed27e96cf30302e16ffa3d28
breucker <breucker@ifris.org>
2014-03-29 21:02:35 +0100

    fighter find enhanced : taking a mongo query as param

--- mcp/server/api/models/fighter.js
+++ mcp/server/api/models/fighter.js
@@ -9,6 +9,7 @@
  var restful = require('node-restful');
  var mongoose = require('mongoose');
 
+ //*** fighter schema ***
  var fighter = mongoose.Schema({
   name: String,
   mailAddress: String,
@@ -20,22 +21,34 @@
   pommelLevel: String,
   refereeLevel: String,
   role: String,
-  description: String
+  description: String,
+  victories: Number
  });
 
-fighter.statics.findByName = function (name, cb){
-    this.find({ name: new RegExp(name, 'i') }, cb);
+//*** fighter methods ***
+
+//find by query : takes a json-formated mongo query and return the record(s) found to callback function (cb)
+fighter.statics.findByQuery = function (query, cb){
+    console.log('find query ', query);
+    
+    this.find(query, cb);
   };
 
+//*** fighter model ***
 var Fighter = restful.model("fighters",fighter).methods(['get', 'delete', 'put', 'post']);
 
 //routes
 Fighter.route('find',function(req, res, next) {
-   console.log('try to find ', req.body.name);
-   this.findByName(req.body.name, function(err,result){
+  var query;
+  if(req.body){
+   console.log('try to find ', req.body.query);
+   this.findByQuery(JSON.parse(req.body.query), function(err,result){
      res.send(result);
    });
-  
+  }
+  else{
+    res.send(400, {error: 'sorry dude ! Missing a request body...'});
+  }
 });
 
 
